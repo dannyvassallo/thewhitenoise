f057e45ac47eb1d50a06a37015b58605232cc003
dannyvassallo <danielvassallo87@gmail.com>
2015-10-20 17:17:08 -0400

    added video and television style effects to the page

---
+++ source/javascripts/_custom.js
@@ -0,0 +1,23 @@
+   
+function rand (min, max) {      
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+ 
+
+setInterval(function() {
+    
+    var randPosOne = rand(0,200),
+        randPosTwo = rand(0,200),
+        bgPosition = randPosOne + 'px ' + randPosTwo + 'px';
+    
+    $('#flicker').css({
+        "background-position": bgPosition
+    });
+    $('#flicker2').css({
+        "background-position": bgPosition
+    });
+    $('#flicker3').css({
+        "background-position": bgPosition
+    });    
+    
+}, 90);
